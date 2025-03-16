8b0692c5c8129941115ba8286c7d6193da6cb875
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2025-03-16 17:16:21 +0300

    feat: implement component with input handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import BooleanTextComponent from "./components/boolean-text-component";
 import CounterComponent from "./components/counter-component";
+import DynamicTextComponent from "./components/dynamic-text-component";
 
 function App() {
   return (
     <section className="section">
       <CounterComponent />
       <BooleanTextComponent text={"kek"} isTextVisible={true} />
+      <DynamicTextComponent />
     </section>
   );
 }
