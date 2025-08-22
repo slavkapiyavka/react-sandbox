32cbba41fdd40b5be8372274da2c5a904281a7ac
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2025-08-23 01:07:40 +0300

    feat: implement ParentComponent

---
+++ src/components/ParentComponent.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const ParentComponent = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseCounter = () => setCounter((v) => ++v);
+  const resetCounter = () => setCounter(0);
+  const setRandomCounter = () => setCounter(Math.round(Math.random() * 10000));
+  const decreaseCounter = () => setCounter((v) => --v);
+
+  return (
+    <section>
+      <p>counter: {counter}</p>
+      <button onClick={increaseCounter} type="button">
+        increase
+      </button>
+      <button onClick={resetCounter} type="button">
+        reset
+      </button>
+      <button onClick={setRandomCounter} type="button">
+        random counter value
+      </button>
+      <button onClick={decreaseCounter} type="button">
+        decrease
+      </button>
+    </section>
+  );
+};
+
+export default ParentComponent;
