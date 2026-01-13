b8c9d7712324214346d5c0cd957cfd78f252e52e
slavkapiyavka <slavkapiyavka@yandex.ru>
2026-01-13 23:32:14 +0300

    feat: handle list item modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
+import { useRef, useState, type FormEvent } from "react";
 import "./App.css";
 import { List } from "./components/List";
 import type { Base } from "./shared/types";
 
 function App() {
-  const list: Base[] = [
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [list, setList] = useState<Base[]>([
     { id: "8453058", text: "text" },
     { id: "4824802", text: "text2" },
     { id: "4324243", text: "text3" },
-  ];
+  ]);
+  const onFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const id = String(Math.floor(Math.random() * 100000000));
+    const text = inputRef.current?.value ?? "";
+
+    setList((l) => [...l, { id, text }]);
+  };
 
   return (
     <>
-      <List items={list} />
+      <form onSubmit={onFormSubmit}>
+        <input type="text" ref={inputRef} />
+        <button type="button" onClick={() => inputRef.current?.focus()}>
+          focus
+        </button>
+      </form>
+      <List
+        items={list}
+        onItemEdit={(item: Base) =>
+          setList((items): Base[] =>
+            items.map((i) =>
+              i.id === item.id ? { id: i.id, text: `!!!${i.text}` } : i,
+            ),
+          )
+        }
+      />
     </>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,23 +1,19 @@
-import { useEffect, useState } from "react";
 import type { Base } from "../shared/types";
 
 type ListProps<T extends Base> = {
   items: T[];
+  onItemEdit: (item: T) => void;
 };
 
-export const List = <T extends Base>({ items }: ListProps<T>) => {
-  const [itemsCopy, setItemsCopy] = useState(items);
-  const onItemTextModify = (item: Base) =>
-    setItemsCopy((prev) =>
-      prev.map((i) => (i.id === item.id ? { ...i, text: `!${i.text}` } : i)),
-    );
+export const List = <T extends Base>({ items, onItemEdit }: ListProps<T>) => {
+  const itemsCopy = items;
 
   return itemsCopy.length ? (
     <ul>
       {itemsCopy.map((item) => (
         <li key={item.id}>
           <span>{item.text}</span>
-          <button type="button" onClick={() => onItemTextModify(item)}>
+          <button type="button" onClick={() => onItemEdit(item)}>
             add `!!!`
           </button>
         </li>
