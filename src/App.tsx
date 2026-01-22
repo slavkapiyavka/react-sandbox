95800da747c7da29b07d8167ba7fb788afec0f76
slavkapiyavka <slavkapiyavka@yandex.ru>
2026-01-22 23:29:49 +0300

    feat: implement `SearchInput` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { useCallback, useState } from "react";
 import "./App.css";
 import CounterButton from "./components/CounterButton";
 import ItemList from "./components/ItemList";
 import SearchInput from "./components/SearchInput";
 
 function App() {
+  const [needle, setNeedle] = useState("");
+  const onChange = useCallback((value: string) => setNeedle(value), []);
+
   return (
     <>
-      <SearchInput />
+      <SearchInput value={needle} onChange={(e) => onChange(e.target.value)} />
       <ItemList />
       <CounterButton />
     </>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,5 +1,9 @@
-const SearchInput = () => {
-  return <input type="text" />;
+import type { ComponentProps } from "react";
+
+type Props = ComponentProps<"input">;
+
+const SearchInput = ({ value, onChange }: Props) => {
+  return <input type="text" value={value} onChange={onChange} />;
 };
 
 export default SearchInput;
