a97de810358931d7b0dd02331bbe6f71a8c3698b
slavkapiyavka <slavkapiyavka@yandex.ru>
2026-02-26 00:38:16 +0300

    feat: render list with `useMemo`

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,24 @@ import "./App.css";
 import CounterButton from "./components/CounterButton";
 import ItemList from "./components/ItemList";
 import SearchInput from "./components/SearchInput";
+import type { ListItem } from "./shared/types/list-item";
 
 function App() {
   const [needle, setNeedle] = useState("");
+  const list: ListItem[] = [
+    { id: 'fn8d', name: 'sirius' },
+    { id: 'v9f890gf', name: 'fang' },
+    { id: 'ni54n', name: 'shelly' },
+    { id: 'k2m4l', name: 'brock' },
+    { id: 'xz90c9', name: 'jessie' },
+    { id: 'ds8dn5', name: 'pam' }
+  ];
   const onChange = useCallback((value: string) => setNeedle(value), []);
 
   return (
     <>
       <SearchInput value={needle} onChange={(e) => onChange(e.target.value)} />
-      <ItemList />
+      <ItemList needle={needle} list={list} />
       <CounterButton />
     </>
   );
--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,5 +1,17 @@
-const ItemList = () => {
-  return <ul>empty list</ul>;
+import { useMemo } from "react";
+import type { ListItem } from "../shared/types/list-item";
+
+interface IProps {
+  needle: string;
+  list: ListItem[];
+}
+
+const ItemList = ({ needle, list }: IProps) => {
+  const filtered = useMemo(() => list.filter((item) => item.name.toLowerCase().includes(needle.toLowerCase())), [needle, list]);
+
+  return (
+    filtered.length ? <ul>{filtered.map((item) => <li key={item.id}>{item.name}</li>)}</ul> : <p>empty list</p>
+  );
 };
 
 export default ItemList;
