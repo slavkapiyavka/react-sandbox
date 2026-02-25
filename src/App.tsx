aba8c7320255c789a348c07e41fdd46ceef7d8a1
slavkapiyavka <slavkapiyavka@yandex.ru>
2026-02-26 01:06:01 +0300

    chore: generate brawlers list

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,12 @@ import "./App.css";
 import CounterButton from "./components/CounterButton";
 import ItemList from "./components/ItemList";
 import SearchInput from "./components/SearchInput";
-import type { ListItem } from "./shared/types/list-item";
+import { BRAWLERS } from "./shared/data/brawlers";
 
 function App() {
   const [needle, setNeedle] = useState("");
   const [counter, setCounter] = useState(0);
 
-  const list: ListItem[] = [
-    { id: 'fn8d', name: 'sirius' },
-    { id: 'v9f890gf', name: 'fang' },
-    { id: 'ni54n', name: 'shelly' },
-    { id: 'k2m4l', name: 'brock' },
-    { id: 'xz90c9', name: 'jessie' },
-    { id: 'ds8dn5', name: 'pam' }
-  ];
   const onChange = useCallback((value: string) => setNeedle(value), []);
   const onIncrement = useCallback(() => setCounter(prev => ++prev), []);
 
@@ -24,7 +16,7 @@ function App() {
     <>
       <p>щётчик: {counter}</p>
       <SearchInput value={needle} onChange={(e) => onChange(e.target.value)} />
-      <ItemList needle={needle} list={list} />
+      <ItemList needle={needle} list={BRAWLERS} />
       <CounterButton onClick={onIncrement} />
     </>
   );
